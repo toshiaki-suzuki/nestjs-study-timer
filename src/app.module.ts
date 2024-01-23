199f601edd7c9fe74efff4150955ccba8800df29
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-23 20:02:55 +0900

    Add MySQL connection library and setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordsModule } from './records/records.module';
 
+const typeOrmModuleOptions: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'test',
+  entities: [],
+  synchronize: true, // true in only dev env
+};
+
 @Module({
-  imports: [RecordsModule],
+  imports: [RecordsModule, TypeOrmModule.forRoot(typeOrmModuleOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
