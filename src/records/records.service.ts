4c646b1701f18f5c92da21a82cd597e3fc4b21fb
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-26 19:55:33 +0900

    Use Repository in RecordService

--- src/records/records.service.ts
+++ src/records/records.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Record } from 'src/entities/record.entity';
+import { Repository } from 'typeorm';
+import { AppDataConfig } from '../data-source';
 
+const dataSource = AppDataConfig;
+const recordRepository = dataSource.getRepository(Record)
 @Injectable()
 export class RecordsService {
-  
-  find(id: string): string {
-		return `find a record ${id}`; 
-	}
+  constructor(
+    @InjectRepository(Record) private readonly recordRepository: Repository<Record>,
+  ) {}
+  async find(id: number): Promise<Record> {
+    return await this.recordRepository.findOneBy({ id });
+  }
 }
