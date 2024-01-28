52f41abb8f174693212c0f0de1f8b83b859ef5c9
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-01-28 18:04:17 +0900

    Add type annotation to find method in RecordsController

--- src/records/records.controller.ts
+++ src/records/records.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { Record } from 'src/entities/record.entity';
 import { RecordsService } from './records.service';
 
 @Controller('records')
@@ -6,7 +7,7 @@ export class RecordsController {
   constructor(private readonly recordsService: RecordsService) {}
 
   @Get(':id')
-  find(@Param('id') id: string):string {
+  find(@Param('id') id: number):Promise<Record> {
       return this.recordsService.find(id); 
   }
 }
