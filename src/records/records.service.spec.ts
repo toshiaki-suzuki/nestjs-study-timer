575fea67c2dea1feced0a12cb88ca286bc0a6ca7
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-02-04 17:13:06 +0900

    Add find test in records spec

--- src/records/records.service.spec.ts
+++ src/records/records.service.spec.ts
@@ -1,52 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import sqlite3 from 'sqlite3';
-import { Column, DataSource, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { Record } from '../entities/record.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Record } from 'src/entities/record.entity';
+import { Repository } from 'typeorm';
 import { RecordsService } from './records.service';
-const { Database } = sqlite3.verbose();
 
-const AppDataSource = new DataSource({
-  type: "sqlite",
-  database: ":memory:",
-  entities: [Record],
-  synchronize: true,
-  logging: false,
-});
-
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
-
-let db: sqlite3.Database;
-beforeAll(done => {
-  db = new Database(':memory:', done);
-  db.serialize(() => {
-    db.run("CREATE TABLE test_table (id INTEGER PRIMARY KEY, value TEXT)");
-  });
-});
-
-afterAll(async () => {
-  // データベースのクローズ
-  db.close();
-});
-
+const mockRecord1 = {
+  id: 1,
+  material: 'test-material',
+  learningTime: 90,
+  description: 'test-description'
+};
 
 describe('RecordsService', () => {
-  let service: RecordsService;
+  let recordsService: RecordsService;
+  let recordsRepository: Repository<Record>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecordsService],
+      providers: [
+        RecordsService,
+        {
+          provide: getRepositoryToken(Record),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<RecordsService>(RecordsService);
+    recordsService = module.get<RecordsService>(RecordsService);
+    recordsRepository = module.get<Repository<Record>>(getRepositoryToken(Record));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('find', () => {
+    it('正常系', async () => {
+      const expected = {
+        ...mockRecord1,
+        createdAt: '',
+        updatedAt: '',
+      };
+
+      jest
+        .spyOn(recordsRepository, 'findOneBy')
+        .mockImplementation(async () => expected);
+      const result = await recordsService.find(1);
+      expect(result).toEqual(expected);
+    });
   });
 });
