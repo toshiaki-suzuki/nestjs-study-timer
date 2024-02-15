a871e43ad69a1e2d7c2d15dec4d8d57aeb297f58
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-02-15 18:58:26 +0900

    delete unnecessary code

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,5 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { Record } from "./entities/record.entity"
 
 export const AppDataConfig = new DataSource({
   type: 'postgres',
