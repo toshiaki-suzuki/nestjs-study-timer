22b5b96a92d48632a9dfc8dbe00a2e4eb6a2d4d1
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-02-17 13:55:17 +0900

    Add create-use dto

---
+++ src/records/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import { Type } from "class-transformer";
+import { IsDateString, IsEmail, IsInt, IsNotEmpty, IsString, MaxLength, } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  name: string;
+
+  @IsEmail()
+  email: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(100)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  birthday: string;
+
+  @IsString()
+  description: string;
+}
