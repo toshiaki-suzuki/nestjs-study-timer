9c1e8f5bf8280b5ae9481837d47fd6e52f997edf
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-02 16:37:12 +0900

    delete unnecessary code

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# ncコマンドを取得
-apt-get update && apt-get install -y netcat-openbsd
-
-# データベースの接続確認
-until nc -z -v -w30 postgres 5432
-do
-  echo "Waiting for database connection..."
-  sleep 1
-done
-echo "Database is up and running"
-
 # マイグレーションの実行
 echo "Running database migrations"
 npx typeorm-ts-node-commonjs migration:run -d src/data-source.ts
