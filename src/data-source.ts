e75c754cbcabfd388e2555963ec967fd8bf956e9
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-04 19:20:02 +0900

    Replace hard coded db parameters with environment variables in data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,16 @@
+import { config } from 'dotenv';
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 
+config(); // 環境変数をロード
+
 export const AppDataConfig = new DataSource({
   type: 'postgres',
-  // host: 'localhost',
-  host: 'postgres',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ["src/entities/*.ts"], 
   migrations: ["migrations/*.ts"]
 });
